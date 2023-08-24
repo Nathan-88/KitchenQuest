02a39b3bd4f3379e52f46a7dfe1a32b0e2241b42
Dev-wonderful <adeonderful20@gmail.com>
2023-08-24 03:18:38 +0100

    recipe section

--- views/scripts/components/navbar.js
+++ views/scripts/components/navbar.js
@@ -2,9 +2,9 @@ export default {
   template: /*html*/ `
     <img src="images/Logo.svg" alt="kitchen Quest">
     <ul>
-      <li>Home</li>
-      <li>Recipes</li>
-      <li>Chefs</li>
+      <li><a href="#">Home</a></li>
+      <li><a href="recipepage.html">Recipes</a></li>
+      <li><a href="#">Trending</a></li>
     </ul>
   `
 };

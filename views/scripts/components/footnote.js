847e36244cf46f7d03c690805cdc708856e82849
Dev-wonderful <adeonderful20@gmail.com>
2023-08-25 02:09:03 +0100

    footer component created and pages updated

---
+++ views/scripts/components/footnote.js
@@ -0,0 +1,45 @@
+export default {
+    template: /*html*/ `
+    <section class="footer-container">
+        <div class="social-info">
+            <img class="logo" src="images/Logo.svg" alt="Kitchen Quest">
+            <p>Nulla in lorem vitae mi ornare porttitor. Fusce nec nisi non ligula.</p>
+            <div class="media-container">
+                <img class="media" src="images/whatsapp.svg" alt="Whatsapp">
+                <img class="media" src="images/twitter.svg" alt="twitter">
+                <img class="media" src="images/instagram.svg" alt="instagram">
+                <img class="media" src="images/pinterest.svg" alt="pinterest">
+            </div>
+            <p class="copyright">2023 Kitchen Q All Rights Reserved</p>
+        </div>
+        <div>
+            <h3>Yoruba Poraro</h3>
+            <div class="extras">
+                <p>About</p>
+                <p>Recipes</p>
+                <p>Trending</p>
+                <p>Explore</p>
+            </div>
+        </div>
+        <div>
+            <h3>Yoruba Poraro</h3>
+            <div class="extras">
+                <p>About</p>
+                <p>Recipes</p>
+                <p>Trending</p>
+                <p>Explore</p>
+            </div>
+        </div>
+        <div>
+            <h3>Yoruba Poraro</h3>
+            <div class="extras">
+                <p>About</p>
+                <p>Recipes</p>
+                <p>Trending</p>
+                <p>Explore</p>
+            </div>
+        </div>
+    </section>
+    `
+  };
+  

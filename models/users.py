c18561fd8ed0a7eaf964735c48b83709e587b702
Dev-wonderful <adeonederful20@gmail.com>
2023-08-29 12:54:11 +0100

    setting up bakend

---
+++ models/users.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defining the schema for users collection"""
+from mongoengine import *
+from models.basemodel import BaseModel
+
+
+class Users(BaseModel, Document):
+    """users Collection in KitchenQuest db"""
+    username = StringField(max_length=120, required=True, unique=True)
+    password = StringField(max_length=120, required=True)
+

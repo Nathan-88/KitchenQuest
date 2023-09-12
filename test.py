6013fd85f1f57a44be8ed430f59a66e10285b52c
Dev-wonderful <adeonederful20@gmail.com>
2023-09-12 01:41:12 +0100

    test deployment

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ save_recipe = {
     'recipe_title': 'Gbegiri Soup'
 }
 
-new_user_response = requests.post('http://localhost:5000/api/v1/user', data=user_details)
+new_user_response = requests.post('https://kitchen-quest-api.onrender.com/api/v1/user', data=user_details)
 print(new_user_response.text)
-verify_user_response = requests.post('http://localhost:5000/api/v1/user/verification', data=user_details)
+verify_user_response = requests.post('https://kitchen-quest-api.onrender.com/api/v1/user/verification', data=user_details)
 print(verify_user_response.text)
-save_recipe_response = requests.post('http://localhost:5000/api/v1/user/save-recipe', data=save_recipe)
+save_recipe_response = requests.post('https://kitchen-quest-api.onrender.com/api/v1/user/save-recipe', data=save_recipe)
 print(save_recipe_response.text)

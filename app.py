a2436de639704b41c01e51a54acab531cce13e71
Dev-wonderful <adeonederful20@gmail.com>
2023-09-12 01:26:50 +0100

    api for the backend

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 """Flask app"""
 from flask import Flask, request
 from models.users import Users
+from argon2 import PasswordHasher
+from mongoengine.errors import *
 import json
+
+passwordhasher = PasswordHasher()
 
 app = Flask(__name__)
 
@@ -10,20 +14,62 @@ def index():
     return 'hello'
 
 
-@app.route('/api/v1/user', methods=['POST'], strict_slashes=False)
+@app.route('/api/v1/user', methods=['POST', 'PUT'], strict_slashes=False)
 def user():
     """save user details"""
     
-    password = request.form.get('password')
-    print(password)
-    username = request.form.get('username')
-    print(username)
-    # data = request.form
-    # print(data)
+    if request.method == 'POST':
+        try:
+            password = request.form.get('password')
+            hashed_password = passwordhasher.hash(password)
+            print(hashed_password)
+            username = request.form.get('username')
+            # print(username)
 
-    new_user = Users(username=username, password=password)
-    new_user.save()
-    return str(new_user.id), 200
+            new_user = Users(username=username, password=hashed_password)
+            new_user.save()
+            return str(new_user.id)
+        except NotUniqueError:
+            return 'Username exists', 409
+
+
+@app.route('/api/v1/user/verification', methods=['POST'], strict_slashes=False)
+def verify_user():
+    """user login authentication"""
+    
+    password = request.form.get('password')
+    username = request.form.get('username')
+    try:
+        user = Users.objects.get(username=username)
+        password_match = passwordhasher.verify(user.password, password)
+        if password_match is True:
+            return 'success'
+        else:
+            return 'Invalid Password', 401
+    except DoesNotExist:
+        return 'User does not exist', 401
+    except Exception as error:
+        print(f'{type(error).__name__}: {error}')
+        return 'An Error has occured while processing your request', 500
+
+
+@app.route('/api/v1/user/save-recipe', methods=['POST'], strict_slashes=False)
+def save_recipe():
+    """save recipe for user"""
+    
+    recipe_title = request.form.get('recipe_title')
+    username = request.form.get('username')
+    try:
+        user = Users.objects.get(username=username)
+        user.saved_recipes.append(recipe_title)
+        user.save()
+    except DoesNotExist:
+        return 'User not logged in', 401
+    except Exception as error:
+        print(f'{type(error).__name__}: {error}')
+        return 'An Error has occured while processing your request', 500
+    
+    return 'success'
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -2,10 +2,17 @@
 import requests
 
 user_details = {
-    'username': 'Wonderful',
+    'username': 'Test User',
     'password': 'Testpassword'
 }
+save_recipe = {
+    'username': 'Test User',
+    'recipe_title': 'Gbegiri Soup'
+}
 
-response = requests.post('http://localhost:5000/api/v1/user', data=user_details)
-# response = requests.get('http://localhost:5000')
-print(response.text)
+new_user_response = requests.post('http://localhost:5000/api/v1/user', data=user_details)
+print(new_user_response.text)
+verify_user_response = requests.post('http://localhost:5000/api/v1/user/verification', data=user_details)
+print(verify_user_response.text)
+save_recipe_response = requests.post('http://localhost:5000/api/v1/user/save-recipe', data=save_recipe)
+print(save_recipe_response.text)
